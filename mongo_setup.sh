fa9a79876a09ff99024609a421cf56f23ab5591d
akshaykishanck <akscher@iu.edu>
2024-11-25 21:21:49 +0000

    fix-setup

--- mongo_setup.sh
+++ mongo_setup.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+sudo apt-get install gnupg curl
+
+curl -fsSL https://www.mongodb.org/static/pgp/server-8.0.asc | \
+   sudo gpg -o /usr/share/keyrings/mongodb-server-8.0.gpg \
+   --dearmor
+
+echo "deb [ signed-by=/usr/share/keyrings/mongodb-server-8.0.gpg ] http://repo.mongodb.org/apt/debian bookworm/mongodb-org/8.0 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-8.0.list
 
 # Update and install MongoDB
 echo "Updating package lists and installing MongoDB..."
